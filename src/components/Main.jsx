6adb80b43a1a4d0978f8cc4e3f4523130b93d424
Joey Pagaduan <joey.paga220@gmail.com>
2023-04-06 19:36:06 -0700

    finally done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import dummyContacts from '../dummyData';
+import ContactList  from './ContactList';
+import SingleContact from './SingleContact';
+
 
 const Main = () => {
+  const [contacts, setContacts] = useState([]);
+  const [selectedContact, setSelectedContact] = useState({});
+
+  const getContacts = async () => {
+    try {
+      const response = await fetch(
+        'http://jsonplace-univclone.herokuapp.com/users'
+      );
+      const result = await response.json();
+      // console.log('Got contacts: ', contacts);
+      setContacts(result);
+      
+    } catch (err) {
+      console.err(error);
+    };
+  }
+
+  const selectContact = async (contactId) => {
+    const response = await fetch(`http://jsonplace-univclone.herokuapp.com/users/${contactId}`);
+    const contact = await response.json();
+    setSelectedContact(contact);
+  }
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        {
+          selectedContact.id ? <SingleContact selectedContact={selectedContact} /> : <ContactList contacts={contacts} selectContact={selectContact} />
+        }
+      </div>
     </div>
   );
+
 };
 
 export default Main;
